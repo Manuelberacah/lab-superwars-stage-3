c47c25c3f19b742ed3d6483a06a32ad3cfe33685
Manuel Beracah <manuel.b@kalvium.community>
2023-12-01 07:55:43 +0530

    update

--- src/app.js
+++ src/app.js
@@ -24,10 +24,18 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = '';
-
+    var i=0
     // Instead of forloop use Map method
     // Code here
-
+    detailedPlayers = players.map(function(item,index){
+        if (index%2==0){
+            type1="hero"
+        }else{
+            type1="villain"
+        }
+        i++
+        return item={name:players[i-1],strength:getRandomStrength(),image:`./images/super-${i}.png`,type:type1} 
+    })
     return detailedPlayers;
 }
 
@@ -39,11 +47,24 @@ const getRandomStrength = () => {
 // Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
-
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    var hero = players.filter(function(item){
+        return item.type=="hero"
+    })
+    var villain = players.filter(function(item){
+        return item.type=="villain"
+    })
+    if (type=="hero"){
+        fragment=hero.map(function(item){
+            return `<div class="player"><img src="${item.image}"alt=""><div class="name">${item.name}</div><div class="strength">${item.strength}</div></div>`
+        }).join("")
+    }else{
+        fragment=villain.map(function(item){
+            return `<div class="player"><img src="${item.image}"alt=""><div class="name">${item.name}</div><div class="strength">${item.strength}</div></div>`
+        }).join("")
+    }
     return fragment;
 }
 
@@ -52,8 +73,6 @@ const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
-
-
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
 }
